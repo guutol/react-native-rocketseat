c1c3fab12d40b568a82bbc9fca8229ea670593d3
Gustavo Bomfim <n4megustavo@gmail.com>
2025-12-15 17:26:06 -0300

    estrutura do projeto organizada

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,9 @@
+import {View, Text, StyleSheet} from 'react-native'
+
+import { styles } from './styles';
+
+export function Home() {
+  return <View style={[styles.container]}>
+    <Text style={[styles.text]}>Hello, world!</Text>
+  </View>
+}
