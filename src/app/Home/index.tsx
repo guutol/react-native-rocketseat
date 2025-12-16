c8432bf540e1df09b68d5f40fb73efba3976a4ad
Gustavo Bomfim <n4megustavo@gmail.com>
2025-12-16 19:04:06 -0300

    adicionando path alias

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -3,6 +3,6 @@ import { styles } from './styles';
 
 export function Home() {
   return <View style={[styles.container]}>
-    <Image source={require("../../assets/logo.png")} style={[styles.logo]}/>
+    <Image source={require("@/assets/logo.png")} style={[styles.logo]}/>
   </View>
 }
