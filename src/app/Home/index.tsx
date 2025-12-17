48778f4a1d14304a477d877fc6afe556fbb8487e
Gustavo Bomfim <n4megustavo@gmail.com>
2025-12-17 16:16:41 -0300

    adição do componente Button

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,8 +1,11 @@
 import {View, Image} from 'react-native'
-import { styles } from './styles';
+import { styles } from './styles'
+import {Button} from '@/components/Button'
+
 
 export function Home() {
   return <View style={[styles.container]}>
-    <Image source={require("@/assets/logo.png")} style={[styles.logo]}/>
+    <Image source={require("@/assets/logo.png")} style={[styles.logo]} />
+    <Button />
   </View>
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -3,9 +3,10 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1, 
-    justifyContent: 'center', 
     alignItems: 'center',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    paddingTop: 62,
+    padding: 24
   },
   logo: {
     height: 34,
