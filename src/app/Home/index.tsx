8ae6cb0f0514eeac4693b09a110ecdcebd13f2cf
guutol <n4megustavo@gmail.com>
2025-12-22 11:42:25 -0300

    adição do componente Input

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -2,13 +2,12 @@ import {View, Image} from 'react-native'
 import { styles } from './styles'
 import {Button} from '@/components/Button'
 
+import {Input} from '@/components/Input'
 
 export function Home() {
   return <View style={[styles.container]}>
     <Image source={require("@/assets/logo.png")} style={[styles.logo]} />
+    <Input placeholder='O que você precisa comprar?' />
     <Button title="Entrar"/>
-    <Button title="Criar conta"/>
-    <Button title="Salvar"/>
-    <Button title="Voltar"/>
   </View>
 }
