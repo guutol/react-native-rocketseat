5975164b3ef3aa0daec391b6dc720ffdcbf62662
guutol <n4megustavo@gmail.com>
2025-12-23 09:50:14 -0300

    adição da seção de conteúdo

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -7,7 +7,13 @@ import {Input} from '@/components/Input'
 export function Home() {
   return <View style={[styles.container]}>
     <Image source={require("@/assets/logo.png")} style={[styles.logo]} />
-    <Input placeholder='O que você precisa comprar?' />
-    <Button title="Entrar"/>
+    <View style={[styles.form]}>
+      <Input placeholder='O que você precisa comprar?' />
+      <Button title="Entrar"/>
+    </View>
+
+    <View style={[styles.content]}>
+
+    </View>
   </View>
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -4,13 +4,26 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1, 
     alignItems: 'center',
-    backgroundColor: 'white',
+    backgroundColor: '#d0d2d8',
     paddingTop: 62,
-    padding: 24,
-    gap: 24
   },
   logo: {
     height: 34,
     width: 134
+  },
+  form: {
+    width: "100%",
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    marginTop: 24,
   }
 });
