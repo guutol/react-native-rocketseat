e47c3f7415b6e36616a660fb7ee53d279ef7f132
guutol <n4megustavo@gmail.com>
2025-12-23 15:51:32 -0300

    adição do botão 'Limpar'

--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -1,15 +1,15 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center',
-    backgroundColor: '#d0d2d8',
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#d0d2d8",
     paddingTop: 62,
   },
   logo: {
     height: 34,
-    width: 134
+    width: 134,
   },
   form: {
     width: "100%",
@@ -24,6 +24,23 @@ export const styles = StyleSheet.create({
     borderTopLeftRadius: 24,
     borderTopRightRadius: 24,
     padding: 24,
+    paddingTop: 32,
     marginTop: 24,
-  }
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E4E6EC",
+    paddingBottom: 12,
+  },
+  clearButton: {
+    marginLeft: "auto",
+  },
+  clearText: {
+    fontSize: 12,
+    color: "#828282",
+    fontWeight: 600,
+  },
 });
