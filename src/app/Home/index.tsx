9203e6aa2cda819b9276e2b0e5b7a2b6261e04a8
Gustavo Bomfim <n4megustavo@gmail.com>
2026-01-23 12:51:59 -0300

    adição de controle de filtros com useState e atualização dinâmica da UI

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, Image, TouchableOpacity, Text, FlatList } from "react-native";
 
 import { Button } from "@/components/Button";
@@ -20,6 +21,8 @@ const ITEMS = [
 ];
 
 export function Home() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+
   console.log("ITEMS", ITEMS);
   return (
     <View style={[styles.container]}>
@@ -32,7 +35,12 @@ export function Home() {
       <View style={[styles.content]}>
         <View style={[styles.header]}>
           {FILTER_STATUS.map((status) => (
-            <Filter key={status} status={status} isActive />
+            <Filter
+              key={status}
+              status={status}
+              isActive={status === filter}
+              onPress={() => setFilter(status)}
+            />
           ))}
           <TouchableOpacity style={[styles.clearButton]}>
             <Text style={[styles.clearText]}>Limpar</Text>
